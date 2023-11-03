3ef09bbaab24513b36dc34c5f7ef6587233d61f7
London Bowen <londonbridge411@gmail.com>
2023-11-03 17:03:28 -0400

    added file

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+// Just for tutorial
+
+
+// PascalCasing
+function Message()
+{
+    // JSX: Short for JavaScript XML (AKA not HTML)
+    return <h1>Hello World</h1>;
+}
+
+// Exports it as a component
+export default Message;
